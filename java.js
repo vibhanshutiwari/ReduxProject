34b493595d02e767c07daf58997561b4577d28ca
vibhanshutiwari <vibhanshtiwari1999@gmail.com>
2020-04-29 20:39:28 +0530

    combine Reducer

--- java.js
+++ java.js
@@ -1,8 +1,9 @@
 const redux = require('redux');
 const createStore = redux.createStore
-
+const combineReducers = redux.combineReducers
 
 const phone = 'numphone';
+const laptop = 'numlaptop';
 
 function sellphone()  {
     return {
@@ -10,8 +11,19 @@ function sellphone()  {
     };
 }
 
+function selllaptop()  {
+    return {
+        type: laptop
+    };
+}
+
+
 const newobject = {
     numofphone: 33
+};
+
+const another = {
+    numoflaptop: 100
 };
 
 const oneReducer = (state = newobject, action) => {
@@ -24,9 +36,26 @@ const oneReducer = (state = newobject, action) => {
    }
 }
 
-const item = createStore(oneReducer);
+const towReducer = (state = another, action) => {
+    switch(action.type) {
+        case laptop : return {
+            ...state,
+            numoflaptop: state.numoflaptop + 25
+        }
+        default: return state
+    }
+ }
+
+const rootReducer = combineReducers({
+  mb: oneReducer,
+  lap: towReducer
+});
+
+const item = createStore(rootReducer);
 console.log('value', item.getState());
 item.dispatch(sellphone());
+item.dispatch(selllaptop());
 item.subscribe(() => console.log('value type', item.getState()));
 item.dispatch(sellphone());
 item.dispatch(sellphone());
+item.dispatch(selllaptop());
